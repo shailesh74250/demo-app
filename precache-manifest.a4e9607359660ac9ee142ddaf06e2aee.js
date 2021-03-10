9953abd7c207ac65ecc648cbfc5e7a641a2288e9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-03-10 12:56:24 +0000

    Deploy shailesh74250/demo-app to github.com/shailesh74250/demo-app.git:gh-pages

---
+++ precache-manifest.a4e9607359660ac9ee142ddaf06e2aee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d1d969586b98d18d49e",
+    "url": "/demo-app/static/css/main.6de65f58.chunk.css"
+  },
+  {
+    "revision": "0d1d969586b98d18d49e",
+    "url": "/demo-app/static/js/main.0d1d9695.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/demo-app/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "28fc3e86c9ea21c389e9",
+    "url": "/demo-app/static/js/runtime~main.28fc3e86.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "36999834aa1b95e7131be235499dd43c",
+    "url": "/demo-app/index.html"
+  }
+];
